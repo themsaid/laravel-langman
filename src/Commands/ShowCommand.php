89ecd7242c8df928b69d20151df08bfa71836ad7
Ferreyra, Maximiliano Gastón <fergthh@gmail.com>
2016-05-21 13:55:32 -0300

    Fixed StyleCI

--- src/Commands/ShowCommand.php
+++ src/Commands/ShowCommand.php
@@ -117,7 +117,6 @@ class ShowCommand extends Command
                     $output[$key]['key'] = $key;
                     $output[$key][$languageKey] = $value;
                 }
-
             }
         }
 
