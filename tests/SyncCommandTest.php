bedbe7030118a6fd3e1e2e9fab866b1f06b71569
Mohamed Said <themohamedsaid@gmail.com>
2016-08-23 20:13:13 +0200

    fix test

--- tests/SyncCommandTest.php
+++ tests/SyncCommandTest.php
@@ -72,7 +72,7 @@ class SyncCommandTest extends TestCase
 
     public function testItDoesntOverrideParentKey()
     {
-        array_map('unlink', glob(__DIR__.'/views_temp/user/index.blade.php'));
+        array_map('unlink', glob(__DIR__.'/views_temp/user.blade.php'));
 
         file_put_contents(__DIR__.'/views_temp/user.blade.php', '{{ trans(\'user.name\') }}');
 
@@ -86,6 +86,6 @@ class SyncCommandTest extends TestCase
 
         $this->assertEquals(['middle' => 'middle'], $userENFile['name']);
 
-        array_map('unlink', glob(__DIR__.'/views_temp/user/index.blade.php'));
+        array_map('unlink', glob(__DIR__.'/views_temp/user.blade.php'));
     }
 }
