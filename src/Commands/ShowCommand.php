307ddeab92c85d94c6b168ffff317c4d3d4b92a2
Pakpoom Yimsukanan <pakpoom.yim@garena.co.th>
2017-11-08 11:09:19 +0700

    Apply StyleCI fix

--- src/Commands/ShowCommand.php
+++ src/Commands/ShowCommand.php
@@ -147,7 +147,7 @@ class ShowCommand extends Command
     private function filesFromKey()
     {
         try {
-            if(Str::contains( $this->file, '/')) {
+            if (Str::contains( $this->file, '/')) {
                 return $this->manager->files()[explode('/', $this->file)[1]];// e.g. 'foo/bar' will return 'bar'
             }
             return $this->manager->files()[$this->file];
