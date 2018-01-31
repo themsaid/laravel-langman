f6f2bbdcd9c456ba7b9c366fbb552f12cbf80564
Mostafa Sedaghat Joo <mostafa.sedaghat@gmail.com>
2018-02-01 01:57:22 +0800

    fix codeing style error

--- src/Commands/UnusedCommand.php
+++ src/Commands/UnusedCommand.php
@@ -74,7 +74,6 @@ class UnusedCommand extends Command
 
             foreach ($languages as $languageKey => $path) {
                 $fileContent = $this->manager->getFileContent($path);
-
                 if (isset($allKeysInFiles[$fileName])) {
                     $missingKeys = array_diff(array_keys(array_dot($fileContent)), $allKeysInFiles[$fileName]);
                     foreach ($missingKeys as $i => $missingKey) {
