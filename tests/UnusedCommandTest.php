ecb44d7d5610bb0a8b20d58d49d9f3231b11ebe6
Mostafa Sedaghat Joo <mostafa.sedaghat@gmail.com>
2018-02-01 01:50:04 +0800

    add language key to report log

--- tests/UnusedCommandTest.php
+++ tests/UnusedCommandTest.php
@@ -8,19 +8,19 @@ class UnusedCommandTest extends TestCase
         array_map('rmdir', glob(__DIR__.'/views_temp/user'));
         array_map('unlink', glob(__DIR__.'/views_temp/user.blade.php'));
 
-        file_put_contents(__DIR__.'/views_temp/user.blade.php', '{{ trans(\'user.name\') }} {{ trans(\'user.age\') }}');
+        file_put_contents(__DIR__.'/views_temp/user.blade.php', "{{ trans('user.name') }} {{ trans('user.age') }}");
         mkdir(__DIR__.'/views_temp/user');
         file_put_contents(__DIR__.'/views_temp/user/index.blade.php', "{{ trans('user.city') }} {{ trans('user.code.initial') }}");
 
         $this->createTempFiles([
             'en' => ['user' => "<?php\n return ['name' => 'Name', 'not_in_files_en' => 'a'];"],
-            'nl' => ['user' => "<?php\n return ['city' => 'city', 'not_in_files_nl' => 'b'];"],
+            'nl' => ['user' => "<?php\n return ['city' => 'city', 'nop' => ['not_in_files_nl' => 'b']];"],
         ]);
 
         $this->artisan('langman:unused');
         
-        $this->assertContains('not_in_files_en', $this->consoleOutput());
-        $this->assertContains('not_in_files_nl', $this->consoleOutput());
+        $this->assertContains('en.not_in_files_en', $this->consoleOutput());
+        $this->assertContains('nl.nop.not_in_files_nl', $this->consoleOutput());
         
 
         array_map('unlink', glob(__DIR__.'/views_temp/user/index.blade.php'));
